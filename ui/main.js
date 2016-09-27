2f674311f8d9023068ac1c0b505b3f81c2ccbc53
abhay180 <abhay180@gmail.com>
2016-09-27 13:32:30 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,12 @@ console.log('Loaded!');
 
 //Move the iamge
 var img= document.getElementbyId('madi');
+//img.style.marginleft = '100px';  
+var marginLeft=0;
+function moveRight() {
+    marginLeft += 10;
+    img.style.marginLeft = marginLeft +'px';
+  }
 img.onclick = function() {
-  img.style.marginleft = '100px';  
+  var interval = setInterval(moveRight,100);
 };
