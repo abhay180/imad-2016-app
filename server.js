cd89b6429cd2af4364b42b3b6b682631182aa170
abhay180 <abhay180@gmail.com>
2016-09-28 15:34:01 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ app.use(morgan('combined'));
 
 
 var articleOne = {
-    title : 'Article one | james Bond',
-    heading : 'Article One',
+    title : 'Article one | James Bond',
+    heading : 'Diamonds are forever',
     date : 'sep 5 2016',
     content : `
               <p>
@@ -17,23 +17,86 @@ var articleOne = {
 };
 
 
+var articleTwo = {
+    title : 'Article Two | James Bond',
+    heading : 'Octopussy',
+    date : 'sep 6 2016',
+    content : `
+              <p>
+	             This is the movie Octopussy! Bond gets to Kamal Khan at Rajasthan!
+	          </p>`
+};
 
+var articleThree = {
+    title : 'Article Three | James Bond',
+    heading : 'Golden Eye',
+    date : 'sep 7 2016',
+    content : `
+              <p>
+	             This is the movie Golden Eye! Bond gets to Sean penn in Cuba!
+	          </p>`
+};
+
+
+function createTemplate (data) {
+    var title  = data.title;
+    var date   = data.date;
+    var heading= data.heading;
+    var content= data.content;
+var htmlTemplate = `<html>
+  <head>
+	  <title> 
+		  ${title}
+          </title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link href="/ui/style.css" rel="stylesheet" />
+   </head>
+   <body>
+       <div class="container">
+           <div>
+	         <a href="/"> Home </a>
+           </div>
+           <hr/>
+           <h3>
+	          ${heading}
+           </h3>
+           <div>
+	          ${date}
+           </div>
+           <div>
+	          ${content}
+           </div>
+        </div>
+        
+    
+   </body>
+</html>`;
+return htmlTempalte;
+
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTempalte(articleOne));
+    
 });
 
-app.get('/article-two', function (req, res) {
-  res.send('Article-two reuested and will be served here');
+app.get('/article-one', function (req, res) {
+  res.send(createTempalte(articleTwo));
+    
 });
 
-app.get('/article-three', function (req, res) {
-  res.send('Article-three reuested and will be served here');
+app.get('/article-one', function (req, res) {
+  res.send(createTempalte(articleThree));
+    
 });
+
+
+
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
