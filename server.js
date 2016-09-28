2311094e0bbce9af60f21c093afe49cdeeb02e16
abhay180 <abhay180@gmail.com>
2016-09-28 16:42:54 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.get('/:articleName', function (req, res) {
 
 
 
-app.get('/ui/style.css', function (req, res) {
+app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
