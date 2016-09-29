b1e56eddeefaded1b5c9f21d2158d2cbd4278086
abhay180 <abhay180@gmail.com>
2016-09-28 17:18:34 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,8 @@ console.log('Loaded!');
 var element = document.getElementById('personal');
 element.innerHTML = 'Hi. My name is Indiana Jones. This is my webapp!';
 
+var element = document.getElementById('prof');
+element.innerHTML = 'I am professor of Archeaology';
 
 //Move the iamge
 var img= document.getElementById('madi');
