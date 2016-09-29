3634a50fe81eedaa548c0768136cb98d5c152c6c
abhay180 <abhay180@gmail.com>
2016-09-29 00:15:23 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,13 +30,26 @@ img.onclick = function() {
   var interval = setInterval(moveLeft,100);
 };
 
-var counter=0;
+
 var button = document.getElementById('counter');
+
 button.onclick = function() {
-    
-    
-    counter++;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+
+  var request = new XMLHttpRequest();
+
+  request.onreadystatechange = function() {
+      if (request.readyState ===XMLHttpRequest.DONE) {
+           if(request.status===200) {
+               var counter = request.responseText;
+               var span = document.getElementById('count');
+               span.innerHTML = counter.toString();
+               
+           }
+             
+      }
+      
+  };
+  request.open('GET', 'http://abhay180.imad.hasura-app.io/counter',true);
+  request.send(null);
 };
 
