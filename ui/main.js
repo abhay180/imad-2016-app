c652ef728dec703c813ab5d09b037245e8e341b9
abhay180 <abhay180@gmail.com>
2016-09-29 00:06:53 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,3 +29,14 @@ function moveRight() {
 img.onclick = function() {
   var interval = setInterval(moveLeft,100);
 };
+
+var counter=0;
+var button = document.getElementById('counter');
+button.onclick = function() {
+    
+    
+    counter++;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+};
+
