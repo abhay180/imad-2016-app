a8a07bd66db07ccd89d79ca161dd658bd8eb0c99
abhay180 <abhay180@gmail.com>
2016-09-29 01:03:33 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,7 +30,7 @@ img.onclick = function() {
   var interval = setInterval(moveLeft,100);
 };
 
-
+/*
 var button = document.getElementById('counter');
 
 button.onclick = function() {
@@ -52,22 +52,35 @@ button.onclick = function() {
   request.open('GET', 'http://abhay180.imad.hasura-app.io/counter',true);
   request.send(null);
 };
-
+*/
 
 //submit name
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
+  
+  var request = new XMLHttpRequest();
+
+  request.onreadystatechange = function() {
+      if (request.readyState ===XMLHttpRequest.DONE) {
+           if(request.status===200) {
+               var names = request.responseText;
+               names = JSON.parse(names);
+               var list = '';
+               for (var i=0;i<names.length; i++) {
+                   list += '<li>' +  names[i] + '</li>'   ;
+               }
+               var ul = document.getElementById('namelist');
+               ul.innerHTML=list; 
+           }
+             
+      }
+  };
     
-    var names = ['name1','name2','name3','name4'];
-    var list = '';
-    for (var i=0;i<names.length; i++) {
-     list += '<li>' +  names[i] + '</li>'   ;
-    }
-    
-    var ul = document.getElementById('namelist');
-    ul.innerHTML=list;
+  request.open('GET', 'http://abhay180.imad.hasura-app.io/submit-name?name='+ name,true);
+  request.send(null);
+};
     
 
 };
